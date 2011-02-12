ee7d666650c782748cacf37b9ee87faa3812dd51
HP_HDX <HP_HDX@192.168.1.112>
2011-02-11 23:34:50 -0800

    PyhsBlockData done

--- src/com/lds/parser/Parser.java
+++ src/com/lds/parser/Parser.java
@@ -1,91 +1,70 @@
-// DOESN'T WORK COMPLETELY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 package com.lds.parser;
 
 import java.io.IOException;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
 import android.content.Context;
 import android.content.res.XmlResourceParser;
 
 import com.lds.game.R;
-
-
-
+ 
 public class Parser
 {
 	
-public XmlPullParserFactory factory;
-public XmlPullParser xpp;
-public XmlResourceParser xrp;   //I have no idea what the difference between the resource and pull parsers is, just how to use them.
+	public XmlResourceParser xrp;  
 
 	public Parser(Context context)
 	{
-		try {
-			factory = XmlPullParserFactory.newInstance();
-		} catch (XmlPullParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		factory.setNamespaceAware(true);
-		
 		xrp = context.getResources().getXml(R.xml.tempdata);
-		try {
-			xpp = factory.newPullParser();
-		} catch (XmlPullParserException e) {
-			
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 	public void parseLevel()
 		throws XmlPullParserException, IOException
 	{
 		
-		//XmlResourceParser tempXML = context.getResources().getXml(R.xml.tempdata);
 		int eventType = xrp.getEventType();
 	
 		while (eventType != XmlPullParser.END_DOCUMENT)
 		{
 			if (eventType == XmlPullParser.START_TAG)
 			{	
-				if(xpp.getName() == "PhysBlock")
+				if(xrp.getName() == "PhysBlock")
 					parsePhysBlock();
 				
-				else if(xpp.getName() == "Player")
+				else if(xrp.getName() == "Player")
 					parsePlayer();
 			}
 			eventType = xrp.next();
 		}
 	}
 	
-	public void parsePhysBlock()
+	public void parsePhysBlock() throws XmlPullParserException, IOException
 	{
-		for (int i = 0; i <= 42; i++)
+		xrp.next();
+		xrp.next();
+		
+		
+		String[] parserPhysBlockA = new String[14];
+		
+		
+		for (int i = 1; i <= 14; i++)
 		{
-			try {
-				xrp.next();
-			} catch (XmlPullParserException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			if ((i + 1) % 3 == 0)
 			{
-				System.out.println(xrp.getText());
+				parserPhysBlockA[i - 1] = xrp.getText();
 			}
-				
+			
+			xrp.next();
+			xrp.next();
+			xrp.next();
 		}
+		
+		new PhysBlockData(parserPhysBlockA);
 	}
 	
 	public void parsePlayer()
 	{
-		
+		//send to a parser and do other shit here		
 	}
 }
--- src/com/lds/parser/PhysBlockData.java
+++ src/com/lds/parser/PhysBlockData.java
@@ -8,26 +8,71 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import com.lds.game.R;*/
+import com.lds.game.R;
 import com.lds.Enums.RenderMode;
 
-/*import android.content.Context;
+import android.content.Context;
 import android.content.res.XmlResourceParser;*/
 
 public class PhysBlockData
 {
-	public int angle;
-	public float size;
-	public float xPos;
-	public float yPos;
-	public float xScale;
-	public float yScale;
-	public boolean isSolid;
-	public int moveSpeed;
-	public int rotateSpeed;
-	public int scaleSpeed;
-	public RenderMode renderMode;
-	public boolean circular;
-	public String color;
-	//public ? texture;
+	private float size, xPos, yPos, xScale, yScale;
+	private int angle, moveSpeed, rotateSpeed, scaleSpeed;
+	private boolean isSolid, circular;
+	private String color;
+
+	public PhysBlockData(String[] xmlPhysValues)
+	{
+		size = Float.parseFloat(xmlPhysValues[1]);
+		xPos = Float.parseFloat(xmlPhysValues[2]);
+		yPos = Float.parseFloat(xmlPhysValues[3]);
+		xScale = Float.parseFloat(xmlPhysValues[4]);
+		yScale = Float.parseFloat(xmlPhysValues[5]);
+		
+		angle = Integer.parseInt(xmlPhysValues[6]);
+		moveSpeed = Integer.parseInt(xmlPhysValues[7]);
+		rotateSpeed = Integer.parseInt(xmlPhysValues[8]);
+		scaleSpeed = Integer.parseInt(xmlPhysValues[9]);
+		
+		isSolid = Boolean.parseBoolean(xmlPhysValues[10]);
+		circular = Boolean.parseBoolean(xmlPhysValues[11]);
+		
+		color = xmlPhysValues[12];
+	}
+	
+	//float setters/getters
+	public void setSize(float newSize) 			{size = newSize;}
+	public void setXPos (float newXPos)			{xPos = newXPos;}
+	public void setYPos (float newYPos)			{yPos = newYPos;}
+	public void setXScale(float newXScale)		{xScale = newXScale;}
+	public void setYScale(float newYScale)		{yScale = newYScale;}
+	
+	public float getSize()			{return size;}
+	public float getXPos() 			{return xPos;}
+	public float getYPos()			{return yPos;}
+	public float getXScale()		{return xScale;}
+	public float getYScale()		{return yScale;}
+
+	//int setters/getters
+	public void setAngle(int newAngle)				{angle = newAngle;}
+	public void setMoveSpeed(int newMoveSpeed)		{moveSpeed = newMoveSpeed;}
+	public void setRotateSpeed(int newRotateSpeed)	{rotateSpeed = newRotateSpeed;}
+	public void setScaleSpeed(int newScaleSpeed)	{scaleSpeed = newScaleSpeed;}
+	
+	public int getAngle()			{return angle;}
+	public int getMoveSpeed()		{return moveSpeed;}
+	public int getRotateSpeed()		{return	rotateSpeed;}
+	public int getScaleSpeed()		{return scaleSpeed;}
+	
+	//bool setters/getters
+	public void setIsSolid(boolean newIsSolid) 		{isSolid = newIsSolid;}
+	public void setCircular(boolean newCircular)	{circular = newCircular;}
+	
+	public boolean getIsSolid()		{return isSolid;}
+	public boolean getCircular()	{return circular;}
+	
+	//String setters/getters
+	public void setColor(String newColor)	{color = newColor;}
+	
+	public String getColor()		{return color;}
 }
