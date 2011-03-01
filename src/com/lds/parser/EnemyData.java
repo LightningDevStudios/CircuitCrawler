645fccabc78246bbde0991b40294379410dce348
HP_HDX <HP_HDX@192.168.1.110>
2011-02-28 19:25:50 -0800

    PARSING WORKS!!!

--- src/com/lds/parser/EnemyData.java
+++ src/com/lds/parser/EnemyData.java
@@ -10,14 +10,15 @@ public class EnemyData extends CharacterData
 	public EnemyData(HashMap<String, String> enemyHM)
 	{
 		super(enemyHM);
-		
-		if(enemyHM.get("type").equalsIgnoreCase("stalker"))
-			type = AIType.STALKER;
-		else if (enemyHM.get("type").equalsIgnoreCase("patrol"))
-			type = AIType.PATROL;
-		else if (enemyHM.get("type").equalsIgnoreCase("turret"))
-			type = AIType.TURRET;
-		
+		if(enemyHM.get("type") != null)
+		{
+			if(enemyHM.get("type").equalsIgnoreCase("stalker"))
+				type = AIType.STALKER;
+			else if (enemyHM.get("type").equalsIgnoreCase("patrol"))
+				type = AIType.PATROL;
+			else if (enemyHM.get("type").equalsIgnoreCase("turret"))
+				type = AIType.TURRET;
+		}
 	}
 	
 	public void setType(AIType newType)		{type = newType;}
