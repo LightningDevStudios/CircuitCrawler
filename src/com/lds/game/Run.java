bb15fcf82441e03ee01b90d3646ffca7ffe77d6c
Matt <Matt@Matt-Desktop>
2011-03-29 00:40:06 -0700

    added radom song thingy

--- src/com/lds/game/Run.java
+++ src/com/lds/game/Run.java
@@ -114,23 +114,46 @@ public class Run extends Activity implements OnGameOverListener, OnGameInitializ
         {
                 public void onCompletion(MediaPlayer arg0) 
                 {
-                	mp.reset();
-                    try 
-                    {
-						mp.setDataSource("/sdcard/song2.mp3");
-					} 
-                    catch (IllegalArgumentException e) 
-                    {
-						e.printStackTrace();
-					} 
-                    catch (IllegalStateException e) 
-                    {
-						e.printStackTrace();
-					} 
-                    catch (IOException e) 
-                    {
-						e.printStackTrace();
-					}
+                	if((Math.random()*2 + 1) == 2)
+                	{
+	                	mp.reset();
+	                    try 
+	                    {
+							mp.setDataSource("/sdcard/song2.mp3");
+						} 
+	                    catch (IllegalArgumentException e) 
+	                    {
+							e.printStackTrace();
+						} 
+	                    catch (IllegalStateException e) 
+	                    {
+							e.printStackTrace();
+						} 
+	                    catch (IOException e) 
+	                    {
+							e.printStackTrace();
+						}
+                	}
+                	else
+                	{
+                		mp.reset();
+	                    try 
+	                    {
+							mp.setDataSource("/sdcard/song1.mp3");
+						} 
+	                    catch (IllegalArgumentException e) 
+	                    {
+							e.printStackTrace();
+						} 
+	                    catch (IllegalStateException e) 
+	                    {
+							e.printStackTrace();
+						} 
+	                    catch (IOException e) 
+	                    {
+							e.printStackTrace();
+						}
+                	}
                     try 
                     {
 						mp.prepare();
