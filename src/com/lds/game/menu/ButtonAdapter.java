6e6e474f3b2a3fcd6a8e8c57556b63610ec52ddd
Matt <Matt@Matt-PC>
2011-03-30 14:52:03 -0700

    TROLOLOLOLOLOLOLOLOLOLOL

--- src/com/lds/game/menu/ButtonAdapter.java
+++ src/com/lds/game/menu/ButtonAdapter.java
@@ -54,6 +54,7 @@ public class ButtonAdapter extends BaseAdapter
 		if (position <= Run.unlockedLevel)
 		{
 			//button.setBackgroundColor(Color.GRAY);
+			button.setImageResource(R.raw.tile1);
 			button.setOnClickListener(new View.OnClickListener()
 			{
 				public void onClick(View v)
