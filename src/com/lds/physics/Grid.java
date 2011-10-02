38a75b5eecb7ad3b9f1b3da5d82c5abe28a6553d
Matt Masarik <matthewmasarik@gmail.com>
2011-10-02 15:00:55 -0700

    Added fixes for entities that fall on lines

--- src/com/lds/physics/Grid.java
+++ src/com/lds/physics/Grid.java
@@ -2,24 +2,27 @@ package com.lds.physics;
 
 import java.util.ArrayList;
 
+import com.lds.game.entity.Entity;
 import com.lds.math.Vector2;
 import com.lds.math.Vector4;
 
 public class Grid 
 {
 	private ArrayList<GridBox> grids = new ArrayList<GridBox>();
+	private ArrayList<Entity> entList;
 	private Grid[] subGrids = new Grid[4];
 	
 	private int level;
 	private Vector2 size;
 	private Vector2 center;
+	private final int MAX_LEVEL = 5;
 	
-	public Grid(Vector2 size, Vector2 center, int level)
+	public Grid(Vector2 size, Vector2 center, int level, ArrayList<Entity> entList)
 	{
 		this.level = level;
 		this.size = size;
 		this.center = center;
-		
+		this.entList = entList;
 		setGrids(CreateGrid(size, center));
 	}
 	
@@ -50,6 +53,7 @@ public class Grid
 			grid.add(new GridBox(new Vector4(X1, X2, Y1, Y2), boxPosList[i], new Vector2(gridSizeX, gridSizeY), "Q" + i));
 		}
 		return grid;
+		
 	}
 
 	public ArrayList<GridBox> getGrids() 
@@ -67,17 +71,67 @@ public class Grid
 		switch(str)
 		{
 			case "Q1":
-				subGrids[0] = new Grid(grids.get(0).gridSize, grids.get(0).center, level + 1);
+				subGrids[0] = new Grid(grids.get(0).gridSize, grids.get(0).center, level + 1, grids.get(0).detectedEnts);
+				FindAndUpdateEntitiesInAQuadrant(subGrids[0]);
 			break;
 			case "Q2":
-				subGrids[1] = new Grid(grids.get(1).gridSize, grids.get(1).center, level + 1);
+				subGrids[1] = new Grid(grids.get(1).gridSize, grids.get(1).center, level + 1, grids.get(1).detectedEnts);
+				FindAndUpdateEntitiesInAQuadrant(subGrids[1]);
 			break;
 			case "Q3":
-				subGrids[2] = new Grid(grids.get(2).gridSize, grids.get(2).center, level + 1);
+				subGrids[2] = new Grid(grids.get(2).gridSize, grids.get(2).center, level + 1, grids.get(2).detectedEnts);
+				FindAndUpdateEntitiesInAQuadrant(subGrids[2]);
 			break;
 			case "Q4":
-				subGrids[3] = new Grid(grids.get(3).gridSize, grids.get(3).center, level + 1);
+				subGrids[3] = new Grid(grids.get(3).gridSize, grids.get(3).center, level + 1, grids.get(3).detectedEnts);
+				FindAndUpdateEntitiesInAQuadrant(subGrids[3]);
 			break;
 		}
+		
+		
+	}
+	
+	public void FindAndUpdateEntitiesInAQuadrant(Grid grid)
+	{
+		for(Entity ent : grid.entList)
+		{
+			boolean isOnALine = true;
+			for(GridBox bx : grid.getGrids())
+			{
+				if(bx.IsInBox(ent))
+				{
+					bx.entityCount++;
+					isOnALine = false;
+				}
+			}
+			if(isOnALine)
+			{
+				//TODO Each entity that runs this if needs to be SATd to all entities in the sub grids of this grid.
+			}
+		}
+		
+		for(GridBox bx : grid.getGrids())
+		{
+			if(bx.entityCount > 1)
+			{
+				if(grid.level <= MAX_LEVEL)
+					grid.SplitGrid(bx.quad);
+				else
+				{
+					//TODO DO SAT on each entity in the bx.
+				}
+			}
+		}
+	}
+	
+	public void Clear()
+	{
+		for(int i = 0; i < 4; i++)
+			subGrids[i].Clear();
+		
+		grids.clear();
+		
+		for(int i = 0; i < 4; i++)
+			subGrids[i] = null;
 	}
 }
--- src/com/lds/physics/GridBroadPhase.java
+++ src/com/lds/physics/GridBroadPhase.java
@@ -1,6 +1,5 @@
 package com.lds.physics;
 
-import java.util.ArrayList;
 import com.lds.math.*;
 import com.lds.game.entity.Entity;
 
@@ -9,38 +8,14 @@ public class GridBroadPhase
 	private World world;	
 	private Grid masterGrid;
 	
-	private final int MinGridSize = 5;
-	
 	public GridBroadPhase(World world) 
 	{
-		masterGrid = new Grid(world.size, new Vector2(0,0), 0);
-	}
-	
-	private void StartBroadPhaseDetection(Grid grid)
-	{
-		//for each box in the grid if it has an entity mark it
-		for(Entity ent : world.entList)
-		{
-			for(GridBox bx : grid.getGrids())
-			{
-				if(bx.IsInBox(ent))
-				{
-					bx.entityCount++;
-				}
-			}
-		}
-		
-		for(GridBox bx : grid.getGrids())
-		{
-			if(bx.entityCount > 1)
-			{
-				grid.SplitGrid(bx.quad);
-			}
-		}
+		masterGrid = new Grid(world.size, new Vector2(0,0), 0, world.entList);
 	}
 	
 	public void Run()
 	{
-		StartBroadPhaseDetection(masterGrid);
+		masterGrid = new Grid(world.size, new Vector2(0,0), 0, world.entList);	
+		masterGrid.FindAndUpdateEntitiesInAQuadrant(masterGrid);
 	}
 }
