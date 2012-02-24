917461d04a5d37b9da1d75b222fd52afabdf4e0f
Robert Rouhani <robert.rouhani@gmail.com>
2012-02-23 20:46:20 -0800

    added a package-info.java back

---
+++ src/com/lds/trigger/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains the classes used in the trigger system for Circuit Crawler.
+ * \todo Move this into com.lds.game?
+ * @author Lightning Development Studios
+ */
+package com.lds.trigger;
